3120e045325487630574796299f4de46738ef59a
Sandy Obare <obaresandy@gmail.com>
2024-11-03 07:06:37 +0300

    0-validate_utf8.py update: refactor validUTF8() function

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,43 +2,23 @@
 """Contains validUTF8 function that validate UTF-8 encoding"""
 
 
-def checkMultiByte(byte):
-    """Checks number of bytes in a multi-byte UTF-8 character"""
-    mask = 15
-    shift = byte >> 4
-
-    if shift & 12 == 12:
-        return 2
-    if shift & 14 == 14:
-        return 3
-    if shift & 15 == 15:
-        return 4
-    return 0
-
-
-def evaluateMultiByte(data, start, bytes):
-    """Evaluates multi-byte UTF-8 characters"""
-    range = start + bytes
-    multiByte = data[start:range]
-    mask = 128
-    for byte in multiByte:
-        if byte & mask != 128:
-            return False
-    return True
-
-
 def validUTF8(data):
     """Validates UTF-8 encoding"""
-    i = 0
-    while i < len(data):
-        byte = data[i] & 0xFF  # Mask to get the 8 least significant bits
-        if byte > 127:
-            bytes_needed = checkMultiByte(byte)
-            if bytes_needed == 0:
+    bytes = 0
+    for num in data:
+        byte = num & 0b11111111
+        if bytes:
+            if byte >> 6 != 0b10:
                 return False
-            if not evaluateMultiByte(data, i + 1, bytes_needed - 1):
-                return False
-            i += bytes_needed
+            bytes -= 1
+            continue
         else:
-            i += 1
-    return True
+            if byte >> 5 == 0b110:
+                bytes = 1
+            elif byte >> 4 == 0b1110:
+                bytes = 2
+            elif byte >> 3 == 0b11110:
+                bytes = 3
+            elif byte >> 7:
+                return False
+    return bytes == 0
