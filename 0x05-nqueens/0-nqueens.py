bc672b249d5e5f53c4a9a5cf71b7b19f3fe9c4b0
Sandy Obare <obaresandy@gmail.com>
2024-11-10 23:58:28 +0300

    0-nqueens.py update: nqueens function implementation

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -16,9 +16,36 @@ if N < 4:
     print("N must be at least 4")
     exit(1)
 
-queens = 0
-positions = []
 
-while queens < N:
-    if len(positions) > 0:
-        i = positions[-1][0]
+def nqueens(N):
+    """N queens puzzle"""
+    queens = []
+    q_cols = []
+
+    def isSafe(x, y):
+        """Check if a queen can be placed on x, y"""
+        for i in range(len(q_cols)):
+            if y == q_cols[i]:
+                return False
+            if x + y == i + q_cols[i] or x - y == i - q_cols[i]:
+                return False
+        return True
+
+    def backtrack(x):
+        """Backtrack"""
+        if x == N:
+            queens.append([[r, c] for r, c in enumerate(q_cols)])
+            return
+
+        for y in range(N):
+            if isSafe(x, y):
+                q_cols.append(y)
+                backtrack(x + 1)
+                q_cols.pop()
+
+    backtrack(0)
+    return queens
+
+
+for board in nqueens(N):
+    print(board)
