0180c44c6e6b5c4074a8c7144686f164947037cb
Sandy Obare <obaresandy@gmail.com>
2024-11-13 20:07:40 +0300

    0-starwars_characters.js semistandard formatting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,22 +5,22 @@ const { argv } = require('node:process');
 const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`
 
 request.get(url, { json: true }, async (err, res, body) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-    if (res.statusCode === 200) {
-        body.characters.forEach(async (character) => {
-            const response = await new Promise((resolve, reject) => {
-                request.get(character, { json: true }, (err, res, body) => {
-                    if (err) {
-                        reject(err);
-                    }
-                    resolve(body);
-                });
-            });
-            console.log(response.name);
+  if (res.statusCode === 200) {
+    body.characters.forEach(async (character) => {
+      const response = await new Promise((resolve, reject) => {
+        request.get(character, { json: true }, (err, res, body) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(body);
         });
-    }
+      });
+      console.log(response.name);
+    });
+  }
 });
