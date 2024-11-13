af4becde6c47ae69d911ea496deff09501b07d95
Sandy Obare <obaresandy@gmail.com>
2024-11-13 20:08:56 +0300

    0-starwars_characters.js more semistandard formatting

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,7 +2,7 @@
 // Write a script that prints all characters of a Star Wars movie:
 const request = require('request');
 const { argv } = require('node:process');
-const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`
+const url = `https://swapi-api.alx-tools.com/api/films/${argv[2]}`;
 
 request.get(url, { json: true }, async (err, res, body) => {
   if (err) {
